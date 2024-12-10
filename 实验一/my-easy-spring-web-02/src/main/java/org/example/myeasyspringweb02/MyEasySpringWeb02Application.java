754ca8e59e044d707713cadb8d316070a1ba1977
JHM2004 <tql_qaq@tju.edu.cn>
2024-12-10 23:41:42 +0800

    Add files via upload
    实验一 done

---
+++ 实验一/my-easy-spring-web-02/src/main/java/org/example/myeasyspringweb02/MyEasySpringWeb02Application.java
@@ -0,0 +1,13 @@
+package org.example.myeasyspringweb02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEasySpringWeb02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEasySpringWeb02Application.class, args);
+	}
+
+}
