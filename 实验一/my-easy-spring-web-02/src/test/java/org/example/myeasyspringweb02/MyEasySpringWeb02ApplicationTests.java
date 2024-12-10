864117d7c80f0ddc2e9cc50ea45e1864ed00d3dc
JHM2004 <tql_qaq@tju.edu.cn>
2024-12-10 23:44:15 +0800

    实验一 done

---
+++ 实验一/my-easy-spring-web-02/src/test/java/org/example/myeasyspringweb02/MyEasySpringWeb02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myeasyspringweb02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEasySpringWeb02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
